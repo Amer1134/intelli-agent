8555855f35228d6fa9cc15c97e8f5141b8d07927
Amer1134 <mohammed234amer@gmail.com>
2026-09-21 16:44:11 +0530

    Build basic LangGraph chat assistant

--- agentic_chatbot_backend.py
+++ agentic_chatbot_backend.py
@@ -1,7 +1,7 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph.message import add_messages
@@ -9,7 +9,10 @@ from langgraph.graph.message import add_messages
 
 load_dotenv()
 
-llm = ChatOpenAI()
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3.6-flash",
+    temperature=0
+)
 
 
 class ChatState(TypedDict):
